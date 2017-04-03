6aa6964f09d53ba579596ebcd85772d1f7addb5b
Deken <deken.7@c-c02p37u0g3qd.ud.osu.edu>
2017-04-03 17:00:02 -0400

    added base files and started dictionary of terms

---
+++ main.js
@@ -0,0 +1,3 @@
+(function(){
+  var dictionary = ['woody','arche', 'oval', 'mirror lake', 'horshoe', 'moritz', 'scarlet', 'gray', 'brutus', 'buckeye leaf', 'ohio state', 'tbdbitl', 'OH-IO', 'script ohio', 'carmen ohio', 'hagerty hall'];
+})();
