27f15cc2598c468a98379de39d7f2fce2b3f7b52
Rory Deken <rorydeken@gmail.com>
2017-04-05 08:31:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 (function(){
-  var dictionary = ['woody','arche', 'oval', 'mirror lake', 'horshoe', 'moritz', 'scarlet', 'gray', 'brutus', 'buckeye leaf', 'ohio state', 'tbdbitl', 'OH-IO', 'script ohio', 'carmen ohio', 'hagerty hall'];
+  var dictionary = ['woody','arche', 'oval', 'mirror lake', 'horshoe', 'moritz', 'scarlet', 'gray', 'brutus', 'buckeye leaf', 'ohio state', 'the union' ,'tbdbitl', 'OH-IO', 'script ohio', 'carmen ohio', 'hagerty hall'];
 })();
