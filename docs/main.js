c0730b52d5f438a8b35f9e169ec1236b8e74afdc
Deken <deken.7@c-c02p37u0g3qd.ud.osu.edu>
2017-04-11 11:26:00 -0400

    updated files

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,15 @@
 (function(){
-  var dictionary = ['woody','arche', 'oval', 'mirror lake', 'horshoe', 'moritz', 'scarlet', 'gray', 'brutus', 'buckeye leaf', 'ohio state', 'the union' ,'tbdbitl', 'OH-IO', 'script ohio', 'carmen ohio', 'hagerty hall'];
+  var dictionary =
+  [
+    'woody','Archie Griff', 'oval', 'mirror lake', 'horseshoe', 'moritz', 'scarlet',
+  'gray', 'Brutus', 'buckeye leaf', 'ohio state', 'the union'
+  ,'tbdbitl', 'OH-IO', 'script ohio', 'carmen ohio', 'Hagerty hall' , 'Fisher', 'John Glenn',
+  'buckeyes' , 'Lee Horvath', 'Michael Redd', 'st. john\'s' ,'excellence', 'Urban Meyer', 'Hoppalong Cassidy', 'Woody Hayes', 'Columbus', 'Orlando Pace', '14-0', 'Olentangy river', 'Les Wexner', 'Mike Conley', 'Ryan Shazier', 'Eddie George', 'Dr. Michael Drake', 'RPAC' , 'Skull Session', '1870'
+  ];
+  var target = document.getElementById("target");
+  for(i = 0; i < dictionary.length; i ++){
+
+    target.innerText = target.innerText + " " + dictionary[i];
+
+  }
 })();
