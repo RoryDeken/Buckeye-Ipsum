c45d3fa027f5446810aa5e7939a8e165f183e8cb
Deken <deken.7@c-c02p37u0g3qd.ud.osu.edu>
2017-04-12 11:07:15 -0400

    bug fix

--- docs/main.js
+++ docs/main.js
@@ -6,11 +6,12 @@
   ,'tbdbitl', 'OH-IO', 'script ohio', 'carmen ohio', 'Hagerty hall' , 'Fisher', 'John Glenn',
   'buckeyes' , 'Lee Horvath', 'Michael Redd', 'st. john\'s' ,'excellence', 'Urban Meyer', 'Hoppalong Cassidy', 'Woody Hayes', 'Columbus', 'Orlando Pace', '14-0', 'Olentangy river', 'Les Wexner', 'Mike Conley', 'Ryan Shazier', 'Eddie George', 'Dr. Michael Drake', 'RPAC' , 'Skull Session', '1870'
   ];
-  var target = document.getElementById("target"), count = document.getElementById('quantity').value;
+  var target = document.getElementById("target"), count;
 
 document.getElementById('submit').addEventListener("click", generate);
 
 function generate(){
+  count = document.getElementById('quantity').value;
   for(i = 0; i < count ; i ++){
 
 
