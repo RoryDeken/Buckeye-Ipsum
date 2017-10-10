ea0171c868bb220262dd943e07ad50b68002a0c6
Deken <deken.7@C-C02P37U0G3QD.local>
2017-10-10 16:49:35 -0400

    fixed some bugs

--- docs/main.js
+++ docs/main.js
@@ -8,52 +8,55 @@
   ,'excellence', 'Urban Meyer', 'Hoppalong Cassidy', 'Woody Hayes', 'Columbus', 'Orlando Pace',
   '14-0', 'Olentangy river', 'Les Wexner', 'Mike Conley', 'Ryan Shazier', 'Eddie George', 'Dr. Michael Drake',
   'RPAC' , 'Skull Session', '1870', 'Jack Nicklaus'
-], target = document.getElementById("target"), count = 0, text, type, limit;
+], target = document.getElementById("target"), count = 0, text, type, limit, randNum = 0;
 
 document.getElementById('submit').addEventListener("click", generate);
 // 90 words = paragraph
 
-   function rand() {Math.floor(Math.random() * dictionary.length - 1);}
+   function generateRandomNumber() {
+
+     return Math.floor(Math.random() * dictionary.length);
+   }
 
 function generate(event){
   event.preventDefault();
   limit = document.getElementById('quantity').value;
   type = parseInt(document.getElementById('type').value);
-  console.log(type);
+  randNum = generateRandomNumber();
+  console.log("Rand num is " + randNum);
+  console.log("The type is: " + type);
+  console.log("Limit is: " + limit);
+count = 0;
   if(type == 1){
 while(count < limit){
-  var randNum = rand();
-count = count + dictionary[randNum].length;
-text = text + " " + dictionary[randNum];
+count++;
+text = text + " " + dictionary[(randNum + count)];
 console.log(randNum);
 }
+randNum = 0;
 }else if (type == 2) {
   for(i = 0; i < limit ; i ++){
 
-var randNum = rand();
-text = text + " " +  dictionary[randNum];
+text = text + " " +  dictionary[(randNum + i)];
 console.log(randNum);
-
-
-
   }
-
+randNum = 0;
 }else {
   for(i = 0; i < limit ; i ++){
-
+randNum++;
     for(j = 0; j < 90 ; j ++){
-var randNum = rand();
-text = text + " " +  dictionary[randNum];
+text = text + " " +  dictionary[(randNum + i)];
 console.log(randNum);
   }
   text = text + "<br />";
 
 
 }
-
+randNum = 0;
+count = 0;
 
 }
-  target.innerText = '';
+  target.innerHTML = '';
 
   target.innerText = text;
 }
